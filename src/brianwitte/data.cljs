f52e72682e796b98392b2ac5858471e21d2905a1
bkz <bkz@dog>
2025-07-01 22:17:38 -0500

    refactors repetitive client code

--- src/brianwitte/data.cljs
+++ src/brianwitte/data.cljs
@@ -5,76 +5,62 @@
 
 (def api-base-url "http://localhost:3000/api")
 
+(def ^:private default-request-opts
+  {:with-credentials? false
+   :headers {"Content-Type" "application/json"}})
+
+(defn- handle-response
+  "Handle API response with consistent error handling"
+  [response error-msg]
+  (if (= 200 (:status response))
+    (:body response)
+    (do
+      (js/console.error error-msg response)
+      {:error error-msg})))
+
+(defn- api-get
+  "Generic API GET request with consistent error handling"
+  [endpoint error-msg & {:keys [log-request?] :or {log-request? false}}]
+  (go
+    (let [url (str api-base-url endpoint)]
+      (when log-request?
+        (js/console.log "Fetching from:" url))
+      (let [response (<! (http/get url default-request-opts))]
+        (when log-request?
+          (js/console.log "Response status:" (:status response))
+          (js/console.log "Response body:" (:body response)))
+        (handle-response response error-msg)))))
+
+;; Public API functions
 (defn fetch-species
   "Fetch all species from the API"
   []
-  (go
-    (js/console.log "Fetching species from:" (str api-base-url "/species"))
-    (let [response (<! (http/get (str api-base-url "/species")
-                                 {:with-credentials? false
-                                  :headers {"Content-Type" "application/json"}}))]
-      (js/console.log "Response status:" (:status response))
-      (js/console.log "Response body:" (:body response))
-      (if (= 200 (:status response))
-        (:body response)
-        (do
-          (js/console.error "Failed to fetch species:" response)
-          {:error "Failed to fetch species"})))))
+  (api-get "/species" "Failed to fetch species" :log-request? true))
 
 (defn fetch-species-by-id
   "Fetch a specific species by ID"
   [id]
-  (go
-    (let [response (<! (http/get (str api-base-url "/species/" id)
-                                 {:with-credentials? false
-                                  :headers {"Content-Type" "application/json"}}))]
-      (if (= 200 (:status response))
-        (:body response)
-        (do
-          (js/console.error "Failed to fetch species:" response)
-          {:error "Failed to fetch species"})))))
+  (api-get (str "/species/" id) "Failed to fetch species"))
 
 (defn fetch-categories
   "Fetch all categories from the API"
   []
-  (go
-    (let [response (<! (http/get (str api-base-url "/categories")
-                                 {:with-credentials? false
-                                  :headers {"Content-Type" "application/json"}}))]
-      (if (= 200 (:status response))
-        (:body response)
-        (do
-          (js/console.error "Failed to fetch categories:" response)
-          {:error "Failed to fetch categories"})))))
+  (api-get "/categories" "Failed to fetch categories"))
 
 (defn search-by-category
   "Search species by category"
   [category]
-  (go
-    (let [response (<! (http/get (str api-base-url "/search/category/" (js/encodeURIComponent category))
-                                 {:with-credentials? false
-                                  :headers {"Content-Type" "application/json"}}))]
-      (if (= 200 (:status response))
-        (:body response)
-        (do
-          (js/console.error "Failed to search by category:" response)
-          {:error "Failed to search"})))))
+  (api-get (str "/search/category/" (js/encodeURIComponent category)) 
+           "Failed to search"))
 
 (defn search-by-name
   "Search species by common name"
   [name]
-  (go
-    (let [response (<! (http/get (str api-base-url "/search/name/" (js/encodeURIComponent name))
-                                 {:with-credentials? false
-                                  :headers {"Content-Type" "application/json"}}))]
-      (if (= 200 (:status response))
-        (:body response)
-        (do
-          (js/console.error "Failed to search by name:" response)
-          {:error "Failed to search"})))))
+  (api-get (str "/search/name/" (js/encodeURIComponent name))
+           "Failed to search"))
 
 ;; For backward compatibility with existing code
 (def data
-  {:species []  ; Will be populated by API calls
+  {:species []     ; Will be populated by API calls
    :categories []
    :loading? true})
